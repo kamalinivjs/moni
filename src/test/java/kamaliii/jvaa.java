e2945f241d2b8775b0ca4d60711739e892e14810
Training_C2a.05.08 <Training_C2a.05.08@CDC2-D-512QSRL.dir.svc.accenture.com>
2019-06-14 17:45:52 +0530

    second

--- src/test/java/kamaliii/jvaa.java
+++ src/test/java/kamaliii/jvaa.java
@@ -2,9 +2,11 @@ package kamaliii;
 
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(monochrome=true,plugin= {"pretty","json:\\src\\test\\java\\kamaliii\\cucumber.reports.json"})
 public class jvaa {
 
 }
